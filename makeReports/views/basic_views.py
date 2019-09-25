12ee4059cd7ced1285081ee6b86b30247626ec82
Sarah <smccarty@unomaha.edu>
2019-09-25 14:35:07 -0500

    Added restricted choice to relevant fields

---
+++ makeReports/views/basic_views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import TemplateView, DetailView
+from django.urls import reverse_lazy, reverse
+from makeReports.models import *
+from makeReports.forms import *
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.conf import settings 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.utils import timezone
+
+class 
+
