d6dacd6b0e28fbf9c71b4d9edb016288507443d0
Sarah <smccarty@unomaha.edu>
2019-09-27 13:06:32 -0500

    Added more views for the forms for SLOs

--- makeReports/views/slo_views.py
+++ makeReports/views/slo_views.py
@@ -475,24 +475,20 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.utils import timezone
 from django.views.generic.edit import FormMixin
 
-class SLOSummary(ListView, FormMixin):
+class SLOSummary(ListView):
     model = SLOInReport
     template_name =""
-    form_class = SLOsToStakeholderEntry
     success_url = ""
     def get_queryset(self):
         report = Report.objects.get(pk=self.request.GET['report'])
         objs = SLOInReport.objects.filter(report=report)
         return objs
-    def form_valid(self,form):
-        report = Report.objects.get(pk=self.request.GET['report'])
-        SLOsToStakeholder(text=form.cleaned_data['text'], report=report)
 class AddNewSLO(FormView):
     template_name = ""
     form_class = CreateNewSLO
     success_url = ""
     def form_valid(self, form):
-        gGoals = forms.cleaned_data["gradGoals"]
+        gGoals = form.cleaned_data["gradGoals"]
         rpt = Report.objects.get(pk=self.request.GET['report'])
         sloObj = SLO.objects.create(blooms=form.cleaned_data['blooms'], gradGoals=form.cleaned_data['gradGoals'])
         sloTxt = SLOText.objects.create(date=datetime.now(), goalText =form.cleaned_data['text'], slo=sloObj)
@@ -505,6 +501,35 @@ class ImportSLO(FormView):
     form_class = ImportSLO
     success_url = ""
     def form_valid(self,form):
-        
+        slo = form.cleaned_data['slo']
+        rpt = Report.objects.get(pk=self.request.GET['report'])
+        SLOText = slo.sloText
+        SLOInReport.objects.creat(sloText=SLOText, firstInstance=False, report=rpt)
+        return super(ImportSLO,self).form_valid(form)
+class EditSLO(FormView):
+    template_name = ""
+    form_class = EditSLO
+    success_url = ""
+    def form_valid(self,form):
+        slo = SLOInReport.objects.get(pk=self.request.GET['sloIR'])
+        if slo.firstInstance:
+             slo.SLOText.goalText = form.cleaned_data['text']
+        else:
+            newTextobj = SLOText.objects.create(date=datetime.now(), goalText=form.cleaned_data['text'], slo = slo.sloText.slo)
+            slo.SLOText=newTextobj
+        slo.save()
+        return super(EditSLO, self).form_valid(form)
+class StakeholderEntry(FormView):
+    template_name = ""
+    form_class = SLOsToStakeholderEntry
+    success_url = ""
+    def form_valid(self,form):
+        rpt = Report.objects.get(pk=self.request.GET['report'])
+        SLOsToStakeholder.objects.create(text=form.cleaned_data['text'], report = rpt)
+        return super(StakeholderEntry,self).form_valid(form)
 
+<<<<<<< HEAD
 >>>>>>> Trying to fix issues with env being committed
+=======
+    
+>>>>>>> Added more views for the forms for SLOs
