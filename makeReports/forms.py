bb785fc8256cc3c86627310fd9bc779922ab2361
Sarah <smccarty@unomaha.edu>
2019-09-28 18:22:59 -0500

    Simplified version control of SLO to match assessment version control

--- makeReports/forms.py
+++ makeReports/forms.py
@@ -32,7 +32,11 @@ class ImportSLO(forms.Form):
         super(ImportSLO, self).__init__(*args, **kwargs)
         sloChoices = kwargs.pop('sloChoices',None)
         self.fields['slo'].queryset = sloChoices
-class EditSLO(forms.Form):
-    text = forms.CharField(widget= forms.Textarea, max_length=600) 
+class EditNewSLO(forms.Form):
+    text = forms.CharField(widget= forms.Textarea, max_length=600)
+    blooms = forms.ChoiceField(choices=BLOOMS_CHOICES, required=False)
+    gradGoals = forms.ModelMultipleChoiceField(queryset=GradGoal.objects.all(), required=False)
+class EditImportedSLO(forms.Form):
+    text = forms.CharField(widget= forms.Textarea, max_length=600)
 class SLOsToStakeholderEntry(forms.Form):
     text = forms.CharField(max_length=2000, widget=forms.Textarea)
