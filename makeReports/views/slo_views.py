427c967b416bfc431b77fab20a9b3c0996cb6434
Sarah <smccarty@unomaha.edu>
2019-09-29 18:28:22 -0500

    Continued improvements and fixes to the SLO pages

--- makeReports/views/slo_views.py
+++ makeReports/views/slo_views.py
@@ -20,28 +20,37 @@ class SLOSummary(ListView):
         report = Report.objects.get(pk=self.kwargs['report'])
         objs = SLOInReport.objects.filter(report=report)
         return objs
+    def get_context_data(self, **kwargs):
+        context = super(SLOSummary, self).get_context_data()
+        context['rptPK'] = self.kwargs['report']
+        return context
 class AddNewSLO(FormView):
     template_name = "makeReports/SLO/addSLO.html"
     form_class = CreateNewSLO
-    success_url = ""
+    #success_url = reverse_lazy('makeReports:slo-summary', rpt=)
+    def get_success_url(self):
+        r = Report.objects.get(pk=self.kwargs['report'])
+        return reverse_lazy('makeReports:slo-summary', args=[r.pk])
     def form_valid(self, form):
         gGoals = form.cleaned_data["gradGoals"]
         rpt = Report.objects.get(pk=self.kwargs['report'])
-        sloObj = SLO.objects.create(blooms=form.cleaned_data['blooms'], gradGoals=form.cleaned_data['gradGoals'])
-        sloRpt = SLOInReport.objects.create(date=datetime.now(), goalText =form.cleaned_data['text'], slo=sloObj, firstInstance= True)
-        sloRpt.report.add(rpt)
+        sloObj = SLO.objects.create(blooms=form.cleaned_data['blooms'])
+        sloObj.gradGoals.set(form.cleaned_data['gradGoals'])
+        sloRpt = SLOInReport.objects.create(date=datetime.now(), goalText =form.cleaned_data['text'], slo=sloObj, firstInstance= True, changedFromPrior=False, report=rpt)
+        sloObj.save()
         sloRpt.save()
         return super(AddNewSLO, self).form_valid(form)
 class ImportSLO(FormView):
     template_name = "makeReports/SLO/importSLO.html"
     form_class = ImportSLOForm
-    success_url = ""
-    def get_form_kwargs(self, index):
-         kwargs = super().get_form_kwargs(index)
-         yearIn = self.kwargs['year']
-         dP = self.kwargs['dp']
-         dPobj = DegreeProgram.objects.get(pk=dP)
-         #rpt = Report.objects.get(year=yearIn, degreeProgram=DegreeProgram.objects.get(pk=dP))
+    def get_success_url(self):
+        r = Report.objects.get(pk=self.kwargs['report'])
+        return reverse_lazy('makeReports:slo-summary', args=[r.pk])
+    def get_form_kwargs(self):
+         kwargs = super(ImportSLO,self).get_form_kwargs()
+         r = Report.objects.get(pk=self.kwargs['report'])
+         yearIn = r.year
+         dPobj = r.degreeProgram
          kwargs['sloChoices'] = SLOInReport.objects.filter(report__year=yearIn, report__degreeProgram=dPobj)
          return kwargs
     def form_valid(self,form):
@@ -52,25 +61,47 @@ class ImportSLO(FormView):
     def get_context_data(self, **kwargs):
         context = super(ImportSLO, self).get_context_data(**kwargs)
         context['currentDPpk'] = Report.objects.get(pk=self.kwargs['report']).degreeProgram.pk
+        context['rpt']=self.kwargs['report']
         return context
 class EditImportedSLO(FormView):
     template_name = "makeReports/SLO/editImportedSLO.html"
     form_class = EditImportedSLOForm
-    success_url = ""
-    def form_valid(self,form):
+    def get_initial(self):
+        initial = super(EditImportedSLO, self).get_initial()
         sloInRpt = SLOInReport.objects.get(pk=self.kwargs['sloIR'])
-        newSLOInRpt = SLOInReport.objects.create(date=datetime.now(), goalText=form.cleaned_data['text'], slo = sloInRpt.slo, changedFromPrior=False, firstInstance=False)
+        initial['text'] = sloInRpt.goalText
+        return initial
+    def get_success_url(self):
+        r = Report.objects.get(pk=self.kwargs['report'])
+        return reverse_lazy('makeReports:slo-summary', args=[r.pk])
+    def form_valid(self,form):
+        r = Report.objects.get(pk=self.kwargs['report'])
+        sloInRpt = SLOInReport.objects.get(pk=self.kwargs['sloIR'])
+        newSLOInRpt = SLOInReport.objects.create(report=r,date=datetime.now(), goalText=form.cleaned_data['text'], slo = sloInRpt.slo, changedFromPrior=False, firstInstance=False)
+        newSLOInRpt.save()
         return super(EditImportedSLO, self).form_valid(form)
 class EditNewSLO(FormView):
     template_name = "makeReports/SLO/editNewSLO.html"
     form_class = EditNewSLOForm
-    success_url = ""
+    def get_initial(self):
+        initial = super(EditNewSLO, self).get_initial()
+        sloInRpt = SLOInReport.objects.get(pk=self.kwargs['sloIR'])
+        initial['text'] = sloInRpt.goalText
+        initial['blooms'] = sloInRpt.slo.blooms
+        initial['gradGoals'] = sloInRpt.slo.gradGoals.all
+        return initial
+    def get_success_url(self):
+        r = Report.objects.get(pk=self.kwargs['report'])
+        return reverse_lazy('makeReports:slo-summary', args=[r.pk])
     def form_valid(self, form):
         sloIR = SLOInReport.objects.get(pk=self.kwargs['sloIR'])
         sloIR.goalText = form.cleaned_data['text']
         sloIR.date = datetime.now()
         sloIR.slo.blooms = form.cleaned_data['blooms']
-        sloIR.slo.gradGoals = form.cleaned_data['gradGoals']
+        sloIR.slo.gradGoals.set(form.cleaned_data['gradGoals'])
+        sloIR.save()
+        sloIR.slo.save()
+        return super(EditNewSLO,self).form_valid(form)
 class StakeholderEntry(FormView):
     template_name = "makeReports/SLO/stakeholdersSLO.html"
     form_class = Single2000Textbox
