ed2b3301270b5e5d5cb176324fe6dc4048fb6346
Sarah <smccarty@unomaha.edu>
2019-10-01 13:32:17 -0500

    Fixed typos with parens

--- makeReports/forms/assessment_forms.py
+++ makeReports/forms/assessment_forms.py
@@ -7,11 +7,11 @@ from django.utils import timezone
 from makeReports.choices import *
 class CreateNewAssessment(forms.Form):
     title = forms.CharField(widget= forms.Textarea, max_length=300)
-    domain = forms.MultipleChoiceField(choices = (("E", "Examination"),("P","Product"),("F","Performance"))
+    domain = forms.MultipleChoiceField(choices = (("E", "Examination"),("P","Product"),("F","Performance")))
     description = forms.CharField(widget=forms.Textarea, max_length=1000)
     finalTerm = forms.ChoiceField(choices = ((True, "In final term"), (False, "In final year")))
     where = forms.CharField(widget= forms.Textarea, max_length=200)
-    allStudents = forms.ChoiceField(choices = ((True, "All Students"), (False,"Sample of Students"))
+    allStudents = forms.ChoiceField(choices = ((True, "All Students"), (False,"Sample of Students")))
     sampleDescription = forms.CharField(widget= forms.Textarea, max_length=200)
     frequency = forms.CharField(widget=forms.Textarea, max_length=100)
     threshold = forms.IntegerField(min_value=0,label="Proficiency Threshold: % of students that meet or exceed expectations")
@@ -22,14 +22,14 @@ class ImportAssessment(forms.Form):
 
 class EditNewAssessment(forms.Form):
     title = forms.CharField(widget= forms.Textarea, max_length=300)
-    domain = forms.MultipleChoiceField(choices = (("E", "Examination"),("P","Product"),("F","Performance"))
+    domain = forms.MultipleChoiceField(choices = (("E", "Examination"),("P","Product"),("F","Performance")))
     description = forms.CharField(widget=forms.Textarea, max_length=1000)
     finalTerm = forms.ChoiceField(choices = ((True, "In final term"), (False, "In final year")))
     where = forms.CharField(widget= forms.Textarea, max_length=200)
-    allStudents = forms.ChoiceField(choices = ((True, "All Students"), (False,"Sample of Students"))
+    allStudents = forms.ChoiceField(choices = ((True, "All Students"), (False,"Sample of Students")))
     sampleDescription = forms.CharField(widget= forms.Textarea, max_length=200)
     frequency = forms.CharField(widget=forms.Textarea, max_length=100)
     threshold = forms.IntegerField(min_value=0,label="Proficiency Threshold: % of students that meet or exceed expectations")
     target = forms.IntegerField(min_value=0, label="Program Proficiency Target: % of students that achieve the proficiency threshold")
 
-class EditImportedAssessment(forms.Form):
+#class EditImportedAssessment(forms.Form):
