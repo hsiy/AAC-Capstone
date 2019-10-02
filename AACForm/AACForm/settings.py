a8f59c4648f0f1ef2d5f0a2bef93d17fd05f9920
beknightedresearch <47165163+beknightedresearch@users.noreply.github.com>
2019-10-01 19:37:09 -0500

    Fixed typo

--- AACForm/AACForm/settings.py
+++ AACForm/AACForm/settings.py
@@ -132,4 +132,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 #GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE= "D:\\School Work\\Senior\\Capstone\\Project\\My Project-08fe48f6125f.json"
-GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE= os.get_env('GOOGLE_DRIVE_PRIVATE')
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE= os.getenv('GOOGLE_DRIVE_PRIVATE')
