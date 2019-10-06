4c909b7371b57028de7f8db006cf37aa5e327d19
David <dream@unomaha.edu>
2019-10-06 03:04:21 -0500

    basic data collection add edit delete summary

--- AACForm/makeReports/views/data_collection_views.py
+++ AACForm/makeReports/views/data_collection_views.py
@@ -39,6 +39,7 @@ class DataCollectionSummary(LoginRequiredMixin,UserPassesTestMixin,ListView):
 
         for assessment in assessments:
             temp_dict = dict()
+            temp_dict['assessment_id'] = assessment.pk
             try:
                 assessment_obj = Assessment.objects.get(pk=assessment.assessment.pk)
                 temp_dict['assessment_text'] = assessment_obj.title
@@ -55,18 +56,25 @@ class DataCollectionSummary(LoginRequiredMixin,UserPassesTestMixin,ListView):
                 assessment_data_obj = AssessmentData.objects.get(assessmentVersion=assessment)
                 temp_dict['num_students_assessed'] = assessment_data_obj.numberStudents
                 temp_dict['overall_proficient'] = assessment_data_obj.overallProficient
+                temp_dict['data_range'] = assessment_data_obj.dataRange
+                temp_dict['assessment_data_id'] = assessment_data_obj.pk
             except:
                 temp_dict['num_students_assessed'] = None
                 temp_dict['overall_proficient'] = None
+                temp_dict['data_range'] = None
+                temp_dict['assessment_data_id'] = None
 
             try:
                 subassessments = Subassessment.objects.filter(assessmentVersion=assessment)
                 temp_dict['subassessments'] = []
                 for subassessment in subassessments:
-                    sub_dict = {subassessment.title : subassessment.proficient}
+                    sub_dict = (subassessment.title, subassessment.proficient)
                     temp_dict['subassessments'].append(sub_dict)
+
+                temp_dict['subassessments_len'] = len(temp_dict['subassessments'])
             except:
                 temp_dict['subassessments'] = []
+                temp_dict['subassessments_len'] = 0
 
             assessment_data_dict['assessments'].append(temp_dict)
             
@@ -78,15 +86,66 @@ class DataCollectionSummary(LoginRequiredMixin,UserPassesTestMixin,ListView):
 
 
 class CreateDataCollectionRow(LoginRequiredMixin,UserPassesTestMixin,FormView):
-    pass
+    template_name = "makeReports/DataCollection/addDataCollection.html"
+    form_class = AddDataCollection
 
+    def dispatch(self, request, *args, **kwargs):
+        self.report = Report.objects.get(pk=self.kwargs['report'])
+        self.assessment = AssessmentVersion.objects.get(pk=self.kwargs['assessment'])
+        return super(CreateDataCollectionRow,self).dispatch(request,*args,**kwargs)
+
+    def get_success_url(self):
+        return reverse_lazy('makeReports:data-summary', args=[self.report.pk])
+
+    def form_valid(self, form):
+        assessmentDataObj = AssessmentData.objects.create(assessmentVersion=self.assessment, dataRange=form.cleaned_data['dataRange'], numberStudents=form.cleaned_data['numberStudents'], overallProficient=form.cleaned_data['overallProficient'])
+        assessmentDataObj.save()
+        return super(CreateDataCollectionRow, self).form_valid(form)
+
+    def test_func(self):
+        return (self.report.degreeProgram.department == self.request.user.profile.department)
 
 class EditDataCollectionRow(LoginRequiredMixin,UserPassesTestMixin,FormView):
-    pass
+    template_name = "makeReports/DataCollection/editDataCollection.html"
+    form_class = EditDataCollection
+
+    def dispatch(self, request, *args, **kwargs):
+        self.report = Report.objects.get(pk=self.kwargs['report'])
+        self.assessment = AssessmentVersion.objects.get(pk=self.kwargs['assessment'])
+        self.dataCollection = AssessmentData.objects.get(pk=self.kwargs['dataCollection'])
+        return super(EditDataCollectionRow,self).dispatch(request,*args,**kwargs)
+
+    def get_initial(self):
+        initial = super(EditDataCollectionRow, self).get_initial()
+        initial['dataRange'] = self.dataCollection.dataRange
+        initial['numberStudents'] = self.dataCollection.numberStudents
+        initial['overallProficient'] = self.dataCollection.overallProficient
+        return initial
+
+    def get_success_url(self):
+        return reverse_lazy('makeReports:data-summary', args=[self.report.pk])
+
+    def form_valid(self, form):
+        self.dataCollection.dataRange = form.cleaned_data['dataRange']
+        self.dataCollection.numberStudents = form.cleaned_data['numberStudents']
+        self.dataCollection.overallProficient = form.cleaned_data['overallProficient']
+        self.dataCollection.save()
+        return super(EditDataCollectionRow, self).form_valid(form)
+
+    def test_func(self):
+        return (self.report.degreeProgram.department == self.request.user.profile.department)
 
 
 class DeleteDataCollectionRow(DeleteView):
-    pass
+    model = AssessmentData
+    template_name = "makeReports/DataCollection/deleteDataCollection.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.report = Report.objects.get(pk=kwargs['report'])
+        return super(DeleteDataCollectionRow,self).dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse_lazy('makeReports:data-summary', args=[self.report.pk])
 
 
 class CreateSubassessment(LoginRequiredMixin,UserPassesTestMixin,FormView):
