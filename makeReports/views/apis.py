4f1aba6d9a0522a3a1e3cecdcfcee673d7888c1f
Sarah <smccarty@unomaha.edu>
2019-10-28 23:14:15 -0500

    Significant progress towards documenting views, and minor clean-up

--- makeReports/views/apis.py
+++ makeReports/views/apis.py
@@ -4,10 +4,16 @@ from rest_framework import serializers
 from makeReports.models import *
 
 class DeptSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializes departments to JSON with the primary key and name
+    """
     class Meta:
         model = Department
         fields = ['pk','name']
 class DeptByColListAPI(generics.ListAPIView):
+    """
+    JSON API to gets active departments within specified college
+    """
     queryset = Department.active_objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = (['college'])
