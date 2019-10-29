30f6866fda2c2ca0bd79a513cbb4a95cb82d8ca1
Sarah <smccarty@unomaha.edu>
2019-10-29 16:53:14 -0500

    Mostly completed documentation

--- AACForm/urls.py
+++ AACForm/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-
+"""
+URLs used on the project level
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include('makeReports.urls')),
