04f08da57dca6f0200cb00355d93d49d2cf5ede2
David <dream@unomaha.edu>
2019-10-31 14:35:13 -0500

    Changed order of choices for convenience

--- AACForm/makeReports/choices.py
+++ AACForm/makeReports/choices.py
@@ -27,7 +27,7 @@ SLO_STATUS_CHOICES = (
     ("Not Met", "Not Met"), 
     ("Unknown", "Unknown"))
 FREQUENCY_CHOICES = (
+    ("O","Other"),
     ("S","Once/semester"),
-    ("Y","Once/year"),
-    ("O","Other")
+    ("Y","Once/year")    
 )
