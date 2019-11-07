f058e311a2124247eb404e435eb9820aad9beec7
ejdargy <edargy@unomaha.edu>
2019-11-07 14:46:08 -0600

    Graphing set-up for generating images

--- AACForm/makeReports/views/apis.py
+++ AACForm/makeReports/views/apis.py
@@ -1,10 +1,14 @@
-from rest_framework import generics
-from django_filters import rest_framework as filters
-from rest_framework import serializers
-from makeReports.models import *
 """
 This file contains the JSON APIs used
 """
+from rest_framework import generics
+from rest_framework import views
+from rest_framework.response import Response
+from django_filters import rest_framework as filters
+from rest_framework import serializers
+from makeReports.models import *
+
+
 class DeptSerializer(serializers.HyperlinkedModelSerializer):
     """
     Serializes departments to JSON with the primary key and name
@@ -48,6 +52,73 @@ class SloByDPListAPI(generics.ListAPIView):
     """
     queryset = SLOInReport.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = (['report__degreeProgram'])
+    filterset_fields = {'report__degreeProgram':['exact'],
+    'report__year':['gte','lte'],
+    }
     serializer_class = SLOserializer
+
+class createGraphAPI(views.APIView):
+    """
+    JSON API to get url of graph on file server with specified college,
+    department, degree program, start and end dates, specific graph choice,
+    and maybe slo to be graphed
+    """
+    def get(self,request,format=None):
+        queryset = SLOStatus.objects.all()
+
+        if 'decision' == 1:
+            print('in decision 1')
+            filter_backends = (filters.DjangoFilterBackend,)
+            filterset_fields = {
+                'report__degreeProgram':['exact'],
+                'report__year':['gte','lte'],
+                'slo':['exact']
+            }
+            
+        elif 'decision' == 2:
+            print('in decision 2')
+            filter_backends = (filters.DjangoFilterBackend,)
+            filterset_fields = {
+                'report__degreeProgram':['exact'],
+                'report__year':['gte','lte']
+            }
+            
+            begYear=request.GET['report__year__gte']
+            endYear = request.GET['report__year__lte']
+            
+            dataFrame = {
+                'Met': [],
+                'Partially Met': [],
+                'Not Met': [],
+                'Unknown': []
+            }
+            index = []
+
+            for year in range(begYear,endYear+1):
+                qYear = queryset.filter(year=year)
+                overall = qYear.count()
+                met = qYear.filter(status=SLO_STATUS_CHOICES[0][0]).count()
+                partiallyMet = qYear.filter(status=SLO_STATUS_CHOICES[1][0]).count()
+                notMet = qYear.filter(status=SLO_STATUS_CHOICES[2][0]).count()
+                unknown = qYear.filter(status=SLO_STATUS_CHOICES[3][0]).count()
+                metP = met/overall
+                parP = partiallyMet/overall
+                notP = notMet/overall
+                unkP = unknown/overall
+                dataFrame['Met'].append(metP)
+                dataFrame['Partially Met'].append(parP)
+                dataFrame['Not Met'].append(notP)
+                dataFrame['Unknown'].append(unkP)
+                index.append(year)
+            
+            df = pd.DataFrame(dataFrame, index=index)
+            lines = df.plot.line()
+            
+        else:
+            print('in decision 3')
+            dataSource['chart'] = {
+                "caption": "Number of Degree Programs Meeting Target"
+            }
+        return Response("hello")
+
 
--- AACForm/makeReports/views/slo_views.py
+++ AACForm/makeReports/views/slo_views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.views.generic.list import ListView
 from django.views.generic.edit import DeleteView, FormView
 from django.urls import reverse_lazy
@@ -460,76 +459,4 @@ class DeleteNewSLO(DeptReportMixin,DeleteView):
         for dA in dAs:
             dA.delete()
         return reverse_lazy('makeReports:slo-summary', args=[self.report.pk])
-=======
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import TemplateView, DetailView
-from django.urls import reverse_lazy, reverse
-from makeReports.models import *
-from makeReports.forms import *
-from datetime import datetime
-from django.contrib.auth.models import User
-from django.conf import settings 
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.utils import timezone
-from django.views.generic.edit import FormMixin
 
-class SLOSummary(ListView):
-    model = SLOInReport
-    template_name =""
-    success_url = ""
-    def get_queryset(self):
-        report = Report.objects.get(pk=self.request.GET['report'])
-        objs = SLOInReport.objects.filter(report=report)
-        return objs
-class AddNewSLO(FormView):
-    template_name = ""
-    form_class = CreateNewSLO
-    success_url = ""
-    def form_valid(self, form):
-        gGoals = form.cleaned_data["gradGoals"]
-        rpt = Report.objects.get(pk=self.request.GET['report'])
-        sloObj = SLO.objects.create(blooms=form.cleaned_data['blooms'], gradGoals=form.cleaned_data['gradGoals'])
-        sloTxt = SLOText.objects.create(date=datetime.now(), goalText =form.cleaned_data['text'], slo=sloObj)
-        sloRpt = SLOInReport.objects.create(sloText=sloTxt, firstInstance= True)
-        sloRpt.report.add(rpt)
-        sloRpt.save()
-        return super(AddNewSLO, self).form_valid(form)
-class ImportSLO(FormView):
-    template_name = ""
-    form_class = ImportSLO
-    success_url = ""
-    def form_valid(self,form):
-        slo = form.cleaned_data['slo']
-        rpt = Report.objects.get(pk=self.request.GET['report'])
-        SLOText = slo.sloText
-        SLOInReport.objects.creat(sloText=SLOText, firstInstance=False, report=rpt)
-        return super(ImportSLO,self).form_valid(form)
-class EditSLO(FormView):
-    template_name = ""
-    form_class = EditSLO
-    success_url = ""
-    def form_valid(self,form):
-        slo = SLOInReport.objects.get(pk=self.request.GET['sloIR'])
-        if slo.firstInstance:
-             slo.SLOText.goalText = form.cleaned_data['text']
-        else:
-            newTextobj = SLOText.objects.create(date=datetime.now(), goalText=form.cleaned_data['text'], slo = slo.sloText.slo)
-            slo.SLOText=newTextobj
-        slo.save()
-        return super(EditSLO, self).form_valid(form)
-class StakeholderEntry(FormView):
-    template_name = ""
-    form_class = SLOsToStakeholderEntry
-    success_url = ""
-    def form_valid(self,form):
-        rpt = Report.objects.get(pk=self.request.GET['report'])
-        SLOsToStakeholder.objects.create(text=form.cleaned_data['text'], report = rpt)
-        return super(StakeholderEntry,self).form_valid(form)
-
-<<<<<<< HEAD
->>>>>>> Trying to fix issues with env being committed
-=======
-    
->>>>>>> Added more views for the forms for SLOs
