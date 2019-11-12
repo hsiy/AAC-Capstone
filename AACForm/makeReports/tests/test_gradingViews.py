15053d012b4ad658061d1fdbfee0643032604cde
Sarah <smccarty@unomaha.edu>
2019-11-12 17:33:21 -0600

    Added a to-do list to each section

--- AACForm/makeReports/tests/test_gradingViews.py
+++ AACForm/makeReports/tests/test_gradingViews.py
@@ -31,5 +31,70 @@ class GradingSectionsTest(ReportAACSetupTest):
         self.assertContains(resp,rI.item.DMEtext)
         self.assertContains(resp,rI.item.MEtext)
         self.assertContains(resp,rI.item.EEtext)
+    def test_sec1_post(self):
+        """
+        Tests that the section 1 grading page works as expected
+        """
+        r = mommy.make("Rubric")
+        rub = mommy.make("GradedRubric",rubricVersion=r)
+        rInG = mommy.make("RubricItem",rubricVersion=r,section=1)
+        rI = mommy.make("GradedRubricItem", rubric=rub, item=rInG)
+        self.rpt.rubric = rub
+        self.rpt.save()
+        resp = self.client.post(reverse('makeReports:grade-sec1',kwargs={'report':self.rpt.pk}),{
+            'rI'+str(rInG.pk):"ME",
+            'section_comment':'fsfkjllaskdfls'
+        })
+        num = GradedRubricItem.objects.filter(rubric=self.rpt.rubric,item=rInG).count()
+        print(num)
+        rI.refresh_from_db()
+        self.assertEquals(rI.grade,"ME")
+        self.assertEquals(rub.section1Comment,'fsfkjllaskdfls')
+        
+    def test_sec2_get(self):
+        """
+        Tests that the section 1 grading page works as expected
+        """
+        r = mommy.make("Rubric")
+        rub = mommy.make("GradedRubric",rubricVersion=r)
+        rInG = mommy.make("RubricItem",rubricVersion=r,section=2)
+        rI = mommy.make("GradedRubricItem", rubric=rub, item=rInG)
+        self.rpt.rubric = rub
+        self.rpt.save()
+        resp = self.client.get(reverse('makeReports:grade-sec2',kwargs={'report':self.rpt.pk}))
+        self.assertContains(resp,rI.item.text)
+        self.assertContains(resp,rI.item.DMEtext)
+        self.assertContains(resp,rI.item.MEtext)
+        self.assertContains(resp,rI.item.EEtext)
+    def test_sec3_get(self):
+        """
+        Tests that the section 1 grading page works as expected
+        """
+        r = mommy.make("Rubric")
+        rub = mommy.make("GradedRubric",rubricVersion=r)
+        rInG = mommy.make("RubricItem",rubricVersion=r,section=3)
+        rI = mommy.make("GradedRubricItem", rubric=rub, item=rInG)
+        self.rpt.rubric = rub
+        self.rpt.save()
+        resp = self.client.get(reverse('makeReports:grade-sec3',kwargs={'report':self.rpt.pk}))
+        self.assertContains(resp,rI.item.text)
+        self.assertContains(resp,rI.item.DMEtext)
+        self.assertContains(resp,rI.item.MEtext)
+        self.assertContains(resp,rI.item.EEtext)
+    def test_sec4_get(self):
+        """
+        Tests that the section 1 grading page works as expected
+        """
+        r = mommy.make("Rubric")
+        rub = mommy.make("GradedRubric",rubricVersion=r)
+        rInG = mommy.make("RubricItem",rubricVersion=r,section=4)
+        rI = mommy.make("GradedRubricItem", rubric=rub, item=rInG)
+        self.rpt.rubric = rub
+        self.rpt.save()
+        resp = self.client.get(reverse('makeReports:grade-sec4',kwargs={'report':self.rpt.pk}))
+        self.assertContains(resp,rI.item.text)
+        self.assertContains(resp,rI.item.DMEtext)
+        self.assertContains(resp,rI.item.MEtext)
+        self.assertContains(resp,rI.item.EEtext)
         
 
---
+++ AACForm/makeReports/views/helperFunctions/todos.py
@@ -0,0 +1,114 @@
+"""
+Generates the to-do list for each section
+"""
+from makeReports.models import *
+from makeReports.forms import *
+from django.contrib.auth.models import User
+
+def section1ToDo(report):
+    """
+    Generates the ToDo list for section 1, includes things missing from the beginning of the report
+
+    Args:
+        report (:class:`~makeReports.models.Report`): in-progress report to generate to-do for
+    Returns:
+        dict, QuerySet : dictionary of to-dos, QuerySet of SLOs in report
+    Notes:
+        Extra return values prevent repeatedly querying the database for the same things
+    """
+    toDos = {
+            'r':[],
+            #"required"
+            's':[]
+            #"suggested"
+        }
+    slos = SLOInReport.objects.filter(report=report)
+    if not report.author:
+        toDos['r'].append("Add author to report")
+    if not report.date_range_of_reported_data:
+        toDos['s'].append("Add date range of reported data")
+    if slos.count() is 0:
+        toDos['r'].append("Create an SLO")
+    if SLOsToStakeholder.objects.filter(report=report).count() is 0:
+        toDos['r'].append("Add description of how SLOs are communicated to stakeholders")
+    return toDos, slos
+def section2ToDo(report):
+    """
+    Generates the to-do list for section 2, inclusive of prior sections
+    
+    Args:
+        report (:class:`~makeReports.models.Report`): in-progress report to generate to-do for
+    Returns:
+        dict, QuerySet, QuerySet : dictionary of to-dos, QuerySet of SLOs in report, QuerySet of assessments in report
+    Notes:
+        Extra return values prevent repeatedly querying the database for the same things
+
+    """
+    toDos, slos = section1ToDo(report)
+    assess = AssessmentVersion.objects.filter(report=report)
+    for slo in slos:
+        if assess.filter(slo=slo).count() is 0:
+            toDos['r'].append("Add an assessment for SLO "+str(slo.number))
+        elif assess.filter(slo=slo, assessment__directMeasure=True).count() is 0:
+            toDos['s'].append("Add a direct measure for SLO "+str(slo.number))
+    return toDos, slos, assess
+def section3ToDo(report):
+    """
+    Generates the to-do list for section 3, including prior sections
+
+    Args:
+        report (:class:`~makeReports.models.Report`): in-progress report to generate to-do for
+    Returns:
+        dict, QuerySet, QuerySet, QuerySet : dictionary of to-dos, QuerySet of SLOs in report, QuerySet of assessments in report, QuerySet of data in report
+    Notes:
+        Extra return values prevent repeatedly querying the database for the same things
+
+    """
+    toDos, slos, assess = section2ToDo(report)
+    data = AssessmentData.objects.filter(assessmentVersion__report=report)
+    for a in assess:
+        if data.filter(assessmentVersion=a).count() is 0:
+            toDos['s'].append("Add data for assessment "+str(a.number))
+        elif AssessmentAggregate.objects.filter(assessmentVersion=a).count() is 0:
+            toDos['s'].append("Add an aggregation of data for assessment "+str(a.number))
+    if ResultCommunicate.objects.filter(report=report).count() is 0:
+        toDos['r'].append("Add description of how results are communicated to stakeholders")
+    return toDos, slos, assess, data
+def section4ToDo(report):
+    """
+    Generates to-do list for section 4, including prior sections
+
+    Args:
+        report (:class:`~makeReports.models.Report`): in-progress report to generate to-do for
+    Returns:
+        dict : dictionary of to-dos
+    Notes:
+        Last section, so extra return values are unneeded
+    """
+    toDos, slos, assess, data = section3ToDo(report)
+    dAs = DecisionsActions.objects.filter(report=report)
+    for slo in slos:
+        if dAs.filter(sloIR=slo).count() is 0:
+            toDos['r'].append("Add a description of decisions and actions relating to SLO "+str(slo.number))
+    return toDos
+def todoGetter(section,report):
+    """
+    Gets the to-do list for given section of a report
+    Normalizes the return value to just the to-do list
+
+    Args:
+        report (:class:`~makeReports.models.Report`): in-progress report to generate to-do for
+        section (int): section number of section to generate list for
+    Returns:
+        dict : dictionary of to-do list, separated into required and suggestions
+    """
+    toDos = None
+    if section is 1:
+        toDos, x = section1ToDo(report)
+    elif section is 2:
+        toDos, x, y = section2ToDo(report)
+    elif section is 3:
+        toDos, x, y, z = section3ToDo(report)
+    elif section is 4:
+        toDos = section4ToDo(report)
+    return toDos
