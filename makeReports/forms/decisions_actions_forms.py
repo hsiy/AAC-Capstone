8608d4a894771413d44abf39cc2b2d770d7f9c7b
Sarah <smccarty@unomaha.edu>
2019-11-15 13:04:39 -0600

    Removed unused decision/action stuff

--- makeReports/forms/decisions_actions_forms.py
+++ makeReports/forms/decisions_actions_forms.py
@@ -6,15 +6,6 @@ from makeReports.models import *
 from .cleaners import CleanSummer
 from django_summernote.widgets import SummernoteWidget
 
-class DecisionsActionsForm(forms.Form):
-    """
-    Form to add or edit decisions/actions with 5 sub text boxes
-    """
-    decisionProcess = forms.CharField(widget= forms.Textarea, max_length=3000, label="Decision Process")
-    decisionMakers = forms.CharField(widget= forms.Textarea, max_length=3000, label="Decision Makers")
-    decisionTimeline = forms.CharField(widget= forms.Textarea, max_length=3000, label="Decision Timeline")
-    dataUsed = forms.CharField(widget= forms.Textarea, max_length=3000, label="Data Used")
-    actionTimeline = forms.CharField(widget= forms.Textarea, max_length=3000, label="Action Timeline")
 class DecActForm1Box(CleanSummer,forms.ModelForm):
     """
     Form to add/edit decision/actions without sub-boxes
