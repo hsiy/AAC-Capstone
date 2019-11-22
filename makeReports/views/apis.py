a79f0ba6634a01b9c5a2b6049e8aaee4693fc3f1
Sarah <smccarty@unomaha.edu>
2019-11-22 00:09:37 -0600

    Added clear override option to data table page

--- makeReports/views/apis.py
+++ makeReports/views/apis.py
@@ -21,6 +21,7 @@ import io
 from datetime import datetime, timedelta
 from matplotlib.ticker import FuncFormatter
 
+
 import pandas as pd
 
 
@@ -161,6 +162,54 @@ class AssessmentBySLO(generics.ListAPIView):
         qS = super(AssessmentBySLO,self).get_queryset()
         qS = qS.order_by('assessment','-report__year').distinct('assessment')
         return qS
+class ClearOverrideAPI(APIView):
+    """
+    Clears the overriden aggregates and SLO statuses
+    """
+    renderer_classes = [JSONRenderer]
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self,request,format=None):
+        """
+        Clears the overridden aggregates and SLO statuses for given report
+
+        Args:
+            request (HttpRequest): the request to the API
+            format (None): not used
+
+        Notes:
+            Expects primary key of report to be passed in GET request as 'pk'
+        """
+        pk = int(request.query_params['pk'])
+        rpt = Report.objects.get(pk=pk)
+        if((rpt.degreeProgram.department==request.user.profile.department) or request.user.profile.aac):
+            #only proceed if the person truly has the right to modify the report
+            statuses = SLOStatus.objects.filter(report__pk=pk, override=True)
+            for status in statuses:
+                status.override = False
+                aggs = AssessmentAggregate.objects.filter(assessmentVersion__slo=status.sloIR)
+                met = True
+                partiallyMet = False
+                for a in aggs:
+                    if a.met is False:
+                        met = False
+                    if a.met is True:
+                        partiallyMet=True
+                    if not met and partiallyMet:
+                        break
+                if met:
+                    status.status = SLO_STATUS_CHOICES[0][0]
+                elif partiallyMet:
+                    status.status = SLO_STATUS_CHOICES[1][0]
+                else:
+                    status.status = SLO_STATUS_CHOICES[2][0]
+                status.save()
+            aggs = AssessmentAggregate.objects.filter(assessmentVersion__report__pk=pk, override=True)
+            for agg in aggs:
+                agg.override = False
+                agg.aggregate_proficiency = calcWeightedAgg(agg.assessmentVersion,0,0)
+                agg.save()
+
 class ImportYearsAPI(APIView):
     """
     Generates list of years a degree program submitted a report
