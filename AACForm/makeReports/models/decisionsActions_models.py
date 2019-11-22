d4ad745432d12c771c5c129d2a81703a98a948bc
Sarah <smccarty@unomaha.edu>
2019-11-22 13:37:24 -0600

    Split models into several files to better match other directories

---
+++ AACForm/makeReports/models/decisionsActions_models.py
@@ -0,0 +1,34 @@
+"""
+This file contains models most directly related to decisions and actions
+"""
+from django.db import models
+from makeReports.choices import *
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
+from gdstorage.storage import GoogleDriveStorage
+from django.core.validators import FileExtensionValidator
+from django.utils.safestring import mark_safe
+import os
+from .basic_models import gd_storage, NonArchivedManager
+
+class DecisionsActions(models.Model):
+    """
+    Model of decisions/actions for a report
+    """
+    SLO = models.ForeignKey('SLO', on_delete=models.CASCADE)
+    report = models.ForeignKey('Report', on_delete=models.CASCADE)
+    sloIR = models.OneToOneField('SLOInReport', on_delete=models.CASCADE)
+    text = models.CharField(max_length=3000, blank=True, default="")
+
+class ReportSupplement(models.Model):
+    """
+    Model to hold supplements to the report as a whole
+    """
+    supplement = models.FileField(
+        upload_to='data/supplements', 
+        storage=gd_storage,
+        validators=[FileExtensionValidator(allowed_extensions=('pdf',))])
+    report = models.ForeignKey('Report', on_delete=models.CASCADE)
+    def __str__(self):
+        return os.path.basename(self.supplement.name)
---
+++ AACForm/makeReports/models/slo_models.py
@@ -0,0 +1,54 @@
+"""
+This file contains models most directly related to Student Learning Outcomes
+"""
+from django.db import models
+from makeReports.choices import *
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
+from gdstorage.storage import GoogleDriveStorage
+from django.core.validators import FileExtensionValidator
+from django.utils.safestring import mark_safe
+import os
+from .basic_models import gd_storage, NonArchivedManager
+
+class SLO(models.Model):
+    """
+    Model collects SLO in reports which are ostensibly  the same except minor changes, 
+    includes only the attributes which should never change and counts how often it is used
+    """
+    blooms = models.CharField(choices=BLOOMS_CHOICES,max_length=50, verbose_name="Bloom's taxonomy level")
+    gradGoals = models.ManyToManyField('GradGoal', verbose_name="graduate-level goals")
+    numberOfUses = models.PositiveIntegerField(default=1, verbose_name="number of uses of this SLO")
+class SLOInReport(models.Model):
+    """
+    A specific version of an SLO which occurs within a report
+    """
+    date = models.DateField()
+    goalText = models.CharField(max_length=1000, verbose_name="goal text")
+    slo = models.ForeignKey(SLO, on_delete=models.CASCADE, verbose_name="SLO")    
+    changedFromPrior = models.BooleanField(verbose_name="changed from prior version")
+    report = models.ForeignKey('Report', on_delete=models.CASCADE)
+    number = models.PositiveIntegerField(default=1)
+    numberOfAssess = models.PositiveIntegerField(default=0, verbose_name="number of assessments")
+    def __str__(self):
+        return self.goalText
+class GradGoal(models.Model):
+    """
+    A graduate goal graduate level programs may obtain
+    """
+    text = models.CharField(max_length=600)
+    active = models.BooleanField(default=True)
+
+    objects = models.Manager()
+    active_objects = NonArchivedManager()
+    def __str__(self):
+        return mark_safe(self.text)
+class SLOsToStakeholder(models.Model):
+    """
+    Text describing how SLOs are communicated to stakeholders
+    """
+    text = models.CharField(max_length=2000)
+    report = models.ForeignKey('Report', on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return mark_safe(self.text)
