27cba848d31bf827f835805dbe9114dd1a365e1b
Sarah <smccarty@unomaha.edu>
2019-12-12 14:40:31 -0600

    Improved the security

--- AACForm/settings.py
+++ AACForm/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os, sys
+import dotenv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ask-+gts^cmc%wnu!1u&*41ed6xysp$hw71i6(+6$5ubpghk4c'
+SECRET_KEY = 'o+4t15z%m*5uj3eoq54dw22i@rwall2aedd6xy2sp$h3w714i6(+6$5ubpghk4c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,16 +85,18 @@ WSGI_APPLICATION = 'AACForm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dec475dtrh1d4b',                      
-        'USER': 'npaddmsaelzkwn',
-        'PASSWORD': '35536c3e563d2356d340584ccdc300cd67bb7e311f86408872183b955b4bb2ff',
-        'HOST': 'ec2-107-21-98-89.compute-1.amazonaws.com',
-        'PORT': '5432',
-    } 
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dec475dtrh1d4b',                      
+#         'USER': 'npaddmsaelzkwn',
+#         'PASSWORD': '35536c3e563d2356d340584ccdc300cd67bb7e311f86408872183b955b4bb2ff',
+#         'HOST': 'ec2-107-21-98-89.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     } 
+# }
+DATABASES = {}
+DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 
 if 'test' in sys.argv:
     #use when wanting PostgreqSQL database for testing
